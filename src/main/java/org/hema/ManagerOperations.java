42a1ae4f7bb266dda264feda19ac3aba636bcb28
Hema <bhemasri56@gmail.com>
2025-06-23 21:14:05 +0530

    operators

---
+++ src/main/java/org/hema/ManagerOperations.java
@@ -0,0 +1,11 @@
+package org.hema;
+
+public class ManagerOperations {
+    public static void main(String[] args) {
+        int n = 1;
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("u r very beautiful");
+        }
+
+    }
+}
--- src/main/java/org/hema/Operations.java
+++ src/main/java/org/hema/Operations.java
@@ -2,8 +2,17 @@ package org.hema;
 
 public class Operations {
     public static void main(String[] args) {
-        System.out.println("Welcome to java program! ");
 
+
+        String message = "Hello World";
+
+        // Length of the string
+        int length = message.length();
+        System.out.println("Length of the string: " + length);
+
+        // Convert to uppercase
+        String uppercaseMessage = message.toUpperCase();
+        System.out.println("Uppercase message: " + uppercaseMessage);
+        System.out.println(message);
     }
 }
-
